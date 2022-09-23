6382b6028a5af2c019ad6ea29511c1220843176f
Kenny Shao <83423941+Kshao09@users.noreply.github.com>
2022-09-23 16:53:15 -0400

    Update pair.c

--- PA1/pair.c
+++ PA1/pair.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
+#include "balance.h"
 
 void pair(char list[]) {
-  
+  for (int i = 0; i < strlen(list); i++) {
+    if (i == strlen(list)) {
+      opening_symbols[i] = '\0';
+      closing_symbols[i] = '\0';
+      break;
+    }
+    else if (list[i] == '(' || list[i] == '{' || list[i] == '[') {
+      opening_symbols[i] = list[i];
+    }
+    else if (list[i] == ')' || list[i] == '}' || list[i] == ']') {
+      closing_symbols[i] = list[i];
+    }
+    else continue;
+  }
 }
