f036eac49ec2466bcd7a8ee3df2397071bc0c64a
Kenny Shao <83423941+Kshao09@users.noreply.github.com>
2022-09-23 17:50:34 -0400

    Update balance.h

--- PA1/balance.h
+++ PA1/balance.h
@@ -1,11 +1,7 @@
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
 
 extern char opening_symbols[51];
 extern char closing_symbols[51];
 int check(char str[]);
 void pair(char list[]);
-int equals(char str1[], char str2[]);
-char get_word(char command[], int max_length);
-char get_line(char command[], int max_length);
